026301b9880165970271b13230dc71549f838638
Brenda <brendinhasal40@gmail.com>
2025-05-30 15:54:54 -0300

    criando variaveis

--- public.tf
+++ public.tf
@@ -1,21 +1,29 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "sa-east-1a"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "comunidadedevops-subnet-1a"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-1a",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
 }
 
 resource "aws_subnet" "eks_subnet_public_1b" {
   vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "sa-east-1b"
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "comunidadedevops-subnet-1b"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "comunidadedevops-subnet-1b",
+      "kubernetes.io/role/elb" = 1
+    }
+  )
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "cidr_block" {
+  type        = string
+  description = "CIDR block que será usado pela VPC"
+}
