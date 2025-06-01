d7c71794fe744dd9736cf06e4f2b179dc073bc26
Brenda <brendinhasal40@gmail.com>
2025-06-01 15:23:42 -0300

    colocando nome do projeto nas variaveis

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "CIDR block que será usado pela VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "nome do projeto que será usado nas tags"
+}
