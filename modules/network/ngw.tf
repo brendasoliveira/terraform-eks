b20600987494aab3a208f051802e7169f43c33dd
Brenda <brendinhasal40@gmail.com>
2025-06-01 16:08:16 -0300

    refactor: criação do módulo network

--- modules/network/ngw.tf
+++ modules/network/ngw.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "eks_ngw_eip_1a" {
   domain           = "vpc"
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name : "${var.project_name}-eip-1a"
     }
@@ -11,7 +11,7 @@ resource "aws_eip" "eks_ngw_eip_1a" {
 resource "aws_eip" "eks_ngw_eip_1b" {
   domain           = "vpc"
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name : "${var.project_name}-eip-1b"
     }
@@ -23,7 +23,7 @@ resource "aws_nat_gateway" "eks_ngw_1a" {
   subnet_id     = aws_subnet.eks_subnet_public_1a.id
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name : "${var.project_name}-ngw-1a"
     }
@@ -35,9 +35,41 @@ resource "aws_nat_gateway" "eks_ngw_1b" {
   subnet_id     = aws_subnet.eks_subnet_public_1b.id
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name : "${var.project_name}-ngw-1b"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1a" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1a.id
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name : "${var.project_name}-priv-route-table-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "eks_private_route_table_1b" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.eks_ngw_1b.id
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name : "${var.project_name}-priv-route-table-1b"
     }
   )
 }
---
+++ modules/network/variables.tf
@@ -0,0 +1,14 @@
+variable "cidr_block" {
+  type        = string
+  description = "CIDR block que será usado pela VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "nome do projeto que será usado nas tags"
+}
+
+variable "tags" {
+  type = map
+  description = "Tags que serão adicionadas aos recursos da AWS"
+}
